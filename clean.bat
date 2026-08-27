2a3956d277893c018d236a803e4112772030c1ef
JiangZhenkun <92253057+nsyhfxc@users.noreply.github.com>
2026-08-27 20:53:24 +0800

    init

---
+++ clean.bat
@@ -0,0 +1,13 @@
+@echo off
+del  *.toc /s
+del  *.out  /s
+del  *.aux  /s
+del  *.log  /s
+del  *.bak  /s
+del  document\*.bak  /s 
+del  texcode\*.bak  /s
+del *.lof /s
+del *.lot /s
+del *.gz /s
+del *.blg /s
+del *.bbl /s
